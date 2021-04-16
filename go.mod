3a21dcc149c883fda587785ea8acd38bc6a7d5cc
Richard Wooding <richard.wooding@gmail.com>
2021-04-16 15:07:13 +0200

    Conway's Game Of Life

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/richardwooding/gameoflife
 
 go 1.16
 
-require github.com/maxence-charriere/go-app/v8 v8.0.1 // indirect
+require (
+	github.com/enescakir/emoji v1.0.0
+	github.com/maxence-charriere/go-app/v8 v8.0.1
+)
--- pkg/life/life.go
+++ pkg/life/life.go
@@ -1,30 +1,31 @@
 package life
 
 import (
-	"fmt"
+	"github.com/enescakir/emoji"
 	"github.com/maxence-charriere/go-app/v8/pkg/app"
-	"io"
+	"time"
 )
 
 type Life struct {
 	app.Compo
 	generation int64
-	dx int
-	dy int
-	colony *[][]bool
+	dx         int
+	dy         int
+	colony     *[][]bool
+	ticker     *time.Ticker
+	done	   chan bool
 }
 
-func New(dx uint, dy uint) *Life {
+func (l *Life) newColony(dx uint, dy uint) {
 	c := make([][]bool, dy)
 	for i := range c {
 		c[i] = make([]bool, dx)
 	}
-	return &Life{
-		generation: 0,
-		dx:   int(dx),
-		dy:   int(dy),
-		colony: &c,
-	}
+	l.generation = 0
+	l.dx = int(dx)
+	l.dy = int(dy)
+	l.colony = &c
+	l.Update()
 }
 
 func (l *Life) count(x int, y int) uint {
@@ -37,55 +38,112 @@ func (l *Life) count(x int, y int) uint {
 	return 0
 }
 
-func  (l *Life) countNeighbours(x int, y int) uint {
+func (l *Life) countNeighbours(x int, y int) uint {
 	return l.count(x-1, y-1) + l.count(x, y-1) + l.count(x+1, y-1) +
-		   l.count(x-1, y)                     + l.count(x+1, y)   +
-		   l.count(x-1, y+1) + l.count(x, y+1) + l.count(x+1, y+1)
+		l.count(x-1, y) + l.count(x+1, y) +
+		l.count(x-1, y+1) + l.count(x, y+1) + l.count(x+1, y+1)
 }
 
-func (l *Life) Generate() {
+func (l *Life) generate() {
 	ng := make([][]bool, l.dy)
 	for i := range ng {
 		ng[i] = make([]bool, l.dx)
 	}
-	for x := 0; x<l.dx; x++ {
-		for y := 0; y<l.dy; y++ {
+	for x := 0; x < l.dx; x++ {
+		for y := 0; y < l.dy; y++ {
 			alive := (*l.colony)[y][x]
 			neighbours := l.countNeighbours(x, y)
-			ng[y][x] =  (alive && (neighbours == 2 || neighbours == 3)) || (!alive && neighbours == 3)
+			ng[y][x] = (alive && (neighbours == 2 || neighbours == 3)) || (!alive && neighbours == 3)
 		}
 	}
 	l.generation++
 	l.colony = &ng
+	l.Update()
 }
 
-func (l *Life) Alive(x int, y int) {
+func (l *Life) alive(x int, y int) {
 	(*l.colony)[y][x] = true
+	l.Update()
 }
 
-func (l *Life) Dead(x int, y int) {
+func (l *Life) dead(x int, y int) {
 	(*l.colony)[y][x] = false
+	l.Update()
 }
 
-func (l *Life) Toggle(x int, y int) {
+func (l *Life) toggle(x int, y int) {
 	(*l.colony)[y][x] = !(*l.colony)[y][x]
+	l.Update()
 }
 
-func (l *Life) Print(w io.Writer) {
-  fmt.Fprintf(w, "Gneration: %d\n", l.generation)
-  for y := range *l.colony {
-  	for x := range (*l.colony)[y] {
-  		if (*l.colony)[y][x] {
-  			fmt.Fprint(w, "*")
-		} else {
-			fmt.Fprint(w, "-")
-		}
+func (l *Life) className(x int, y int) string {
+	if (*l.colony)[y][x] {
+		return "alive"
+	} else {
+		return "dead"
 	}
-	fmt.Fprintln(w)
-  }
 }
 
-func (l *Life) GenerateAndPrint(w io.Writer) {
-	l.Generate()
-	l.Print(w)
+func (l *Life) startTicking(ctx app.Context) {
+	l.ticker = time.NewTicker(50*time.Millisecond)
+	l.done = make(chan bool)
+	ctx.Async(func() {
+		for {
+			select {
+			case <-l.done:
+				return
+			case <- l.ticker.C:
+				l.Defer(func(context app.Context) {
+					l.generate()
+				})
+			}
+		}
+	})
+}
+
+func (l *Life) stopTicking() {
+	l.ticker.Stop()
+	l.ticker = nil
+	l.Update()
+}
+
+func (l *Life) Render() app.UI {
+
+	var colony [][]bool
+	if l.colony == nil {
+		colony = [][]bool{}
+	} else {
+		colony = *l.colony
+	}
+
+	return app.Div().Body(
+		app.H1().Text("Game of life"),
+		app.If(l.colony == nil,
+			app.Button().Text("Make Colony").OnClick(func(ctx app.Context, e app.Event) {
+				l.newColony(64, 64)
+			}),
+		).Else(
+			app.If(l.ticker == nil,
+				app.Button().Text(emoji.PlayButton).OnClick(func(ctx app.Context, e app.Event) {
+					l.startTicking(ctx)
+				}),
+			).Else(
+				app.Button().Text(emoji.PauseButton).OnClick(func(ctx app.Context, e app.Event) {
+					l.stopTicking()
+				}),
+			),
+			app.Hr(),
+			app.Div().Class("wrapper").Body(
+				app.Range(colony).Slice(func(y int) app.UI {
+					return app.Range(colony[y]).Slice(func(x int) app.UI {
+						return app.Div().Class(l.className(x, y)).OnClick(func(ctx app.Context, e app.Event) {
+							if l.ticker == nil {
+								l.toggle(x, y)
+							}
+						})
+					})
+				}),
+			),
+		),
+	)
 }
